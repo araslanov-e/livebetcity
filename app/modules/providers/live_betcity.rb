99bbe0ad8d35db5781c4a4210bbd293357fac7d5
araslanov <araslanov_e_i@list.ru>
2012-12-14 03:27:27 +0600

    Add additional lines

--- app/modules/providers/live_betcity.rb
+++ app/modules/providers/live_betcity.rb
@@ -5,23 +5,24 @@ module Providers
     URL_LIVE_BETS = "http://betcityru.com/live/line.php" # live ставки
     URL_ALL_LIVE_BETS = "http://betcityru.com/livebetssh.php" # все live ставки
     URL_LANGUAGE = "http://betcityru.com/lngswitch.php?lang=%s" # выбор языка
-    MAIN_PERIOD = {
-      "American Football" => "-1",
-      "Badminton" => "0",
-      "Bandy" => "0",
-      "Baseball" => "0",
-      "Basketball" => "-1",
-      "Darts" => "0",
-      "Futsal" => "0",
-      "Handball" => "0",
-      #"Hockey" => 0, -1
-      "Hockey" => "0",
-      "Rugby Union" => "0",
-      "Snooker" => "0",
-      "Table Tennis" => "-1",
-      "Tennis" => "-1",
-      "Volleyball" => "0",
-      "Soccer" => "0"
+    # Разновидности спорта
+    # содержат период в основное время, значение которого зависит от X.present? (есть ничья) [true, false]
+    SPORTS = {
+      "American Football" => ["-1","-1"],
+      "Badminton" => ["0","0"],
+      "Bandy" => ["0","0"],
+      "Baseball" => ["0","0"],
+      "Basketball" => ["-1","-1"],
+      "Darts" => ["0","0"],
+      "Futsal" => ["0","0"],
+      "Handball" => ["0","0"],
+      "Hockey" => ["0","-1"],
+      "Rugby Union" => ["0","0"],
+      "Snooker" => ["0","0"],
+      "Table Tennis" => ["-1","-1"],
+      "Tennis" => ["-1","-1"],
+      "Volleyball" => ["0","0"],
+      "Soccer" => ["0","0"]
     }
 
     def initialize(sport = :soccer)
@@ -50,8 +51,9 @@ module Providers
     end
 
     # период для основного времени
-    def get_main_period(sport)
-      MAIN_PERIOD[sport]
+    # draw - true/false
+    def get_main_period(sport, draw = false)
+      draw ? SPORTS[sport][0] : SPORTS[sport][1]
     end
 
     private
@@ -59,6 +61,45 @@ module Providers
       def initial_request
         @agent = Mechanize.new
         @agent.get(URL_LANGUAGE % 'en') # выбор английского языка
+      end
+
+      def parse_handicaps(line, home_team_or_first_player, away_team_or_second_player)
+        # Handicap: BK Prostejov: (-10.5) 1.9; BK Decin: (+10.5) 1.8;
+        home_team_or_first_player_matches = line.match(/Handicap:\s?#{Regexp.escape(home_team_or_first_player)}:\s\(([\+\-\d\.]+)\)\s([\d\.]+)/)
+        handicap_1, value_1 = home_team_or_first_player_matches ? [home_team_or_first_player_matches[1], home_team_or_first_player_matches[2]] : [nil] * 2
+
+        away_team_or_second_player_matches = line.match(/[:;]\s?#{Regexp.escape(away_team_or_second_player)}:\s\(([\+\-\d\.]+)\)\s([\d\.]+)/)
+        handicap_2, value_2 = away_team_or_second_player_matches ? [away_team_or_second_player_matches[1], away_team_or_second_player_matches[2]] : [nil] * 2
+
+        [handicap_1.gsub(/[+]/, ''), value_1, handicap_2.gsub(/[+]/, ''), value_2]
+      end
+
+      def parse_totals(line)
+        # Total: (134.5) Under 1.8; Over 1.9;
+        total_matches = line.match(/Total:\s\(([\d\.]+)\)\s?(Under\s([\d\.]+);)?\s?(Over\s([\d\.]+);)?/)
+        total_matches ? [total_matches[1], total_matches[3], total_matches[5]] : [nil] * 3
+      end
+
+      def parse_ind_totals(line, home_team_or_first_player, away_team_or_second_player)
+        # Ind. Total: BK Prostejov: (71.5) Under 1.87; Over 1.87; BK Decin: (62) Under 1.87; Over 1.87;
+        home_team_or_first_player_matches = line.match(/Ind\.\sTotal:\s?#{Regexp.escape(home_team_or_first_player)}:\s\(([\d\.]+)\)\s?(Under\s([\d\.]+);)?\s?(Over\s([\d\.]+);)?/)
+        total_1, under_1, over_1 = home_team_or_first_player_matches ? [home_team_or_first_player_matches[1], home_team_or_first_player_matches[3], home_team_or_first_player_matches[5]] : [nil] * 3
+
+        away_team_or_second_player_matches = line.match(/[:;]\s+#{Regexp.escape(away_team_or_second_player)}:\s\(([\d\.]+)\)\s?(Under\s([\d\.]+);)?\s?(Over\s([\d\.]+);)?/)
+        total_2, under_2, over_2 = away_team_or_second_player_matches ? [away_team_or_second_player_matches[1], away_team_or_second_player_matches[3], away_team_or_second_player_matches[5]] : [nil] * 3
+
+        [total_1, under_1, over_1, total_2, under_2, over_2]
+      end
+
+      def get_totals_over_period(names, values)
+        period_line = {} # хеш значений периода
+        names.each_with_index do |name, index|
+          # в первом столбце - период
+          column_name = index.zero? ? "period" : name.inner_text.force_encoding("BINARY").gsub(/\xA0|\xC2/, '').force_encoding("UTF-8") # удаляем \xA0 и \xC2
+          next if column_name.empty? # пропускаем другие пустые столбцы
+          period_line[column_name] = values[index].inner_text.gsub(/[+]/, '')
+        end
+        period_line
       end
 
   end
--- app/modules/providers/live_betcity/all.rb
+++ app/modules/providers/live_betcity/all.rb
@@ -18,14 +18,16 @@ module Providers
             # TODO не всегда название лиги полностью переведены
             league = sport_and_league_array.drop(1).join(". ").strip # остальные элементы, кроме первого - название лиги
 
+            next if SPORTS[sport].blank? # пропускаем, если информация по данному спорту не нужны
+
             result[sport] ||= {}
             result[sport][league] ||= {}
 
             # events
             table.search(".lbk").each do |event|
 
-              main_line_values = event.search("td")
-              main_line = {}
+              main_line_values = event.search("td") # значения основного периода
+              main_line = {} # хеш значений основного периода
               n = 1
               event.previous_element.search("td").each_with_index do |name, index|
                 # изменяем одинаковые названия полей
@@ -37,23 +39,29 @@ module Providers
                 end
                 main_line[column_name] = main_line_values[index].inner_text.gsub(/[+]/, '')
               end
-              # puts main_line
 
               # datetime event, home team, away team
               datetime_event = "#{Date.today.to_s}-#{main_line["Time"]}"
               home_team = main_line["Team 1"]
               away_team = main_line["Team 2"]
-              full_name = "#{home_team}, #{away_team}, #{datetime_event}"
+              first_player = main_line["Player 1"]
+              second_player = main_line["Player 2"]
+
+              # команды или игроки
+              home_team_or_first_player = home_team || first_player
+              away_team_or_second_player = away_team || second_player
+
+              full_name = "#{home_team_or_first_player}, #{away_team_or_second_player}, #{datetime_event}"
               result[sport][league][full_name] ||= []
 
               # totals
               totals = []
-              home_team_wins = main_line["X"].blank? ? "ML1" : "1" # теннис, волейбол, баскетбол
-              away_team_wins = main_line["X"].blank? ? "ML2" : "2" # теннис, волейбол, баскетбол
-              main_period = get_main_period(sport) # период для основного времени
+              home_team_or_first_player_wins = main_line["X"].present? ? "1" : "ML1" # теннис, волейбол, баскетбол
+              away_team_or_second_player_wins = main_line["X"].present? ? "2" : "ML2" # теннис, волейбол, баскетбол
+              main_period = get_main_period(sport, main_line["X"].present?) # период для основного времени
 
-              totals << [main_period, home_team_wins, nil, main_line["1"]] if main_line["1"].present?
-              totals << [main_period, away_team_wins, nil, main_line["2"]] if main_line["2"].present?
+              totals << [main_period, home_team_or_first_player_wins, nil, main_line["1"]] if main_line["1"].present?
+              totals << [main_period, away_team_or_second_player_wins, nil, main_line["2"]] if main_line["2"].present?
               totals << [main_period, "1X", nil, main_line["1X"]] if main_line["1X"].present?
               totals << [main_period, "12", nil, main_line["12"]] if main_line["12"].present?
               totals << [main_period, "X2", nil, main_line["X2"]] if main_line["X2"].present?
@@ -61,6 +69,74 @@ module Providers
               totals << [main_period, "F2", main_line["Handicap 2"], main_line["Odds2"]] if main_line["Handicap 2"].present?
               totals << [main_period, "TO", main_line["Total"], main_line["Over"]] if main_line["Over"].present?
               totals << [main_period, "TU", main_line["Total"], main_line["Under"]] if main_line["Under"].present?
+
+              # additional lines
+              lines_nodes = event.next_element # tr
+
+              # handicaps
+              lines_nodes.search('./td/div[b="Handicap:"]').each do |h|
+                handicap_1, value_1, handicap_2, value_2 = parse_handicaps(h.content, home_team_or_first_player, away_team_or_second_player)
+                totals << [main_period, "F1", handicap_1, value_1] if handicap_1 && value_1 # handicap 1
+                totals << [main_period, "F2", handicap_2, value_2] if handicap_2 && value_2 # handicap 2
+              end
+
+              # totals
+              lines_nodes.search('./td/div[b="Total:"]').each do |t|
+                total, under, over = parse_totals(t.content)
+                if total # notice: totals can be the same as in the basic line
+                  totals << [main_period, "TO", total, over] if over
+                  totals << [main_period, "TU", total, under] if under
+                end
+              end
+
+              # both to score
+              lines_nodes.search('./td/div[b="Both teams to score or one scoreless:"]').each do |bts|
+                bts_matches = bts.content.match(/Both score:\s([\d\.]+); One scoreless:\s([\d\.]+)/)
+                bts_y_value, bts_n_value = bts_matches ? [bts_matches[1], bts_matches[2]] : nil * 2
+                totals << [main_period, "BTS_Y", nil, bts_y_value] if bts_y_value
+                totals << [main_period, "BTS_N", nil, bts_n_value] if bts_n_value
+              end
+
+              # even/odd
+              lines_nodes.search('./td/div[b="Even/Odd Total:"]').each do |event_odd|
+                event_odd_matches = event_odd.content.match(/Even:\s([\d\.]+); Odd:\s([\d\.]+)/)
+                even_value, odd_value = event_odd_matches ? [event_odd_matches[1], event_odd_matches[2]] : nil * 2
+                totals << [main_period, "EVEN", nil, even_value] if even_value
+                totals << [main_period, "ODD", nil, odd_value] if odd_value
+              end
+
+              # individual totals
+              lines_nodes.search('./td/div[b="Ind. Total:"]').each do |it|
+                total_1, under_1, over_1, total_2, under_2, over_2 = parse_ind_totals(it.content, home_team_or_first_player, away_team_or_second_player)
+                # home_team_or_first_player
+                if total_1
+                  totals << [main_period, "I1TO", total_1, over_1] if over_1
+                  totals << [main_period, "I1TU", total_1, under_1] if under_1
+                end
+                # away_team_or_second_player
+                if total_2
+                  totals << [main_period, "I2TO", total_2, over_2] if over_2
+                  totals << [main_period, "I2TU", total_2, under_2] if under_2
+                end
+              end
+
+              # периоды
+              # search("table#dt")
+              #lines_nodes.search('./td/table[@id="dt"]').each do |periods|
+              #  # строки таблицы
+              #  columns_name = []
+              #  periods.search("tr").each_with_index do |line, number_line|
+              #    case number_line
+              #      when 0 # title table
+              #        next
+              #      when 1 # column name
+              #        columns_name = line.search("td.tdh")
+              #      else # other line
+              #        period_totals = line.search("td")
+              #        puts get_totals_over_period(columns_name, period_totals)
+              #    end
+              #  end
+              #end
 
               result[sport][league][full_name] = totals
 
